aa72ef970a3b2dfa52eb6672901653d101b6d486
ritabc <ritabc@gmail.com>
2018-09-24 22:47:25 -0700

    Fix bug in cart_items helper method

--- app/helpers/cart_helper.rb
+++ app/helpers/cart_helper.rb
@@ -1,7 +1,8 @@
 module CartHelper
   def cart_items
+    order_items = OrderItem.all
     cart_quantity = 0
-    @order_items.each do |oi|
+    order_items.each do |oi|
       cart_quantity += oi.quantity
     end
     cart_quantity
